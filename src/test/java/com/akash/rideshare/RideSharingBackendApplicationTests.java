73fe24d6e034baed22b69009c049bc4ce29c57e5
Akash Verma <akashazad9941@gmail.com>
2026-07-10 20:58:19 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/akash/rideshare/RideSharingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.rideshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSharingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
